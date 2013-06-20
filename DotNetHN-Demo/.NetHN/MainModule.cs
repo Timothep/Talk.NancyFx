ddab19dcb4f54c45bd5d8d77d275e62ad8381cd6
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-20 10:06:43 +0200

    Minimal changes

--- DotNetHN-Demo/.NetHN/MainModule.cs
+++ DotNetHN-Demo/.NetHN/MainModule.cs
@@ -1,44 +1,40 @@
-﻿namespace NetHN
-{
-	using System.Collections.Generic;
-	using System.Dynamic;
-	using System.Linq;
-	using model;
-	using Nancy;
-	using Nancy.Authentication.Forms;
-	using Nancy.Extensions;
-	using Simple.Data;
-	using Simple.Data.MongoDB;
-	using Nancy.ModelBinding;
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using NetHN.model;
+using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Extensions;
+using Simple.Data;
+using Simple.Data.MongoDB;
+using Nancy.ModelBinding;
 
+namespace NetHN
+{
 	public class MainModule : NancyModule
 	{
 		public MainModule()
 		{
 			// Root of the application, gather all submissions
-			Get["/"] =
-				_ =>
+			Get["/"] = _ =>
 				{
 					return "Nancy HackerNews Main Page";
 				};
 
 			// Redirection to the "submit" page
-			Get["/submit"] =
-				_ =>
+			Get["/submit"] = _ =>
 				{
 					return "Submit a new link";
 				};
 
 			// Post action upon submit
-			Post["/submit"] =
-				_ =>
+			Post["/submit"] = _ =>
 				{
 					return "Process the new submission";
 				};
 
 			// Increase the score of the given submission
-			Get["/voteup/{id}"] =
-				parameters =>
+			Get["/voteup/{id}"] = parameters =>
 				{
 					return "Vote up submission " + parameters.id;
 				};
--- DotNetHN-Demo/.NetHN/model/Submission.cs
+++ DotNetHN-Demo/.NetHN/model/Submission.cs
@@ -1,73 +1,75 @@
 ﻿namespace NetHN.model
 {
-	using System;
-	using System.Globalization;
+    using System;
+    using System.Globalization;
 
-	public class Submission
-	{
-		private readonly Random rand = new Random(DateTime.Now.Millisecond);
+    public class Submission
+    {
+        private readonly Random rand = new Random(DateTime.Now.Millisecond);
 
-		public string Id { get; set; }
-		private string url;
-		public string Url
-		{
-			get { return url; }
-			set
-			{
-				// Set via autobinding, 
-				//  only chance we have 
-				//  to implicitely set
-				//  the other parameters
-				if (value.StartsWith("www."))
-					url = "http://" + value;
-				else
-					url = value;
-				Source = GetDomainFromUrl();
-				Votes = 0;
-				Id = GetNewStringId();
-			}
-		}
-		public string Title { get; set; }
-		public string Source { get; set; }
-		public int Votes { get; set; }
-		public string User { get; set; }
-		public long Timestamp { get; set; }
-		public int Origin { get; set; }
-		public string Name { get; set; }
+        public string Id { get; set; }
 
-		/// <summary>
-		/// Fills up the fields that could not be automagically bound
-		/// </summary>
-		public void SetUserContext(string userLogin = "")
-		{
-			this.User = !string.IsNullOrEmpty(userLogin) ? userLogin : "an0nymous";
-		}
+        private string url;
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                // Set via autobinding, 
+                //  only chance we have 
+                //  to implicitely set
+                //  the other parameters
+                if (value.StartsWith("www."))
+                    url = "http://" + value;
+                else
+                    url = value;
+                Source = GetDomainFromUrl();
+                Votes = 0;
+                Id = GetNewStringId();
+            }
+        }
 
-		/// <summary>
-		/// Returns a new ID
-		/// </summary>
-		private string GetNewStringId()
-		{
-			return rand.Next().ToString(CultureInfo.InvariantCulture);
-		}
+        public string Title { get; set; }
+        public string Source { get; set; }
+        public int Votes { get; set; }
+        public string User { get; set; }
+        public long Timestamp { get; set; }
+        public int Origin { get; set; }
+        public string Name { get; set; }
 
-		/// <summary>
-		/// Extracts the domain from a full URL
-		/// </summary>
-		/// <returns>The domain part of the URL if found, an empty string otherwise</returns>
-		private string GetDomainFromUrl()
-		{
-			Uri givenUri;
-			string host;
+        /// <summary>
+        ///   Fills up the fields that could not be automagically bound
+        /// </summary>
+        public void SetUserContext(string userLogin = "")
+        {
+            User = !string.IsNullOrEmpty(userLogin) ? userLogin : "an0nymous";
+        }
 
-			if (Uri.TryCreate(this.Url, UriKind.Absolute, out givenUri))
-				host = givenUri.Host;
-			else
-				throw new ArgumentException("Wrong URL format");
+        /// <summary>
+        ///   Returns a new ID
+        /// </summary>
+        private string GetNewStringId()
+        {
+            return rand.Next().ToString(CultureInfo.InvariantCulture);
+        }
 
-			host = host.Remove(0, 4);
+        /// <summary>
+        ///   Extracts the domain from a full URL
+        /// </summary>
+        /// <returns>The domain part of the URL if found, an empty string otherwise</returns>
+        private string GetDomainFromUrl()
+        {
+            Uri givenUri;
+            string host;
 
-			return host;
-		}
-	}
+            if (Uri.TryCreate(Url, UriKind.Absolute, out givenUri))
+                host = givenUri.Host;
+            else
+                throw new ArgumentException("Wrong URL format");
+
+            host = host.Remove(0, 4);
+
+            return host;
+        }
+    }
 }
