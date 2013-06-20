fd9faf068804e6494fcad2e230b74a693b73359e
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-20 10:43:46 +0200

    Starting point of the demo

---
+++ Demo/Demo/MainModule.cs
@@ -0,0 +1,34 @@
+﻿namespace Demo
+{
+    using Nancy;
+
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            // Root of the application, gather all submissions
+            Get["/"] = _ =>
+            {
+                return "Nancy HackerNews Main Page";
+            };
+
+            // Redirection to the "submit" page
+            Get["/submit"] = _ =>
+            {
+                return "Submit a new link";
+            };
+
+            // Post action upon submit
+            Post["/submit"] = _ =>
+            {
+                return "Process the new submission";
+            };
+
+            // Increase the score of the given submission
+            Get["/voteup/{id}"] = parameters =>
+            {
+                return "Vote up submission " + parameters.id;
+            };
+        }
+    }
+}
