d728985ed4b43ffd551cb36a567275d1c8b46096
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-24 00:41:30 +0200

    Small changes to the self hosted demo project

--- Demo_NancyInitial/NancyDemo/MainModule.cs
+++ Demo_NancyInitial/NancyDemo/MainModule.cs
@@ -8,30 +8,39 @@ namespace NancyDemo
 {
     public class MainModule : NancyModule
     {
+        private const string connectionString = @"mongodb://localhost:27017/dotnethn";
+        private dynamic db = Database.Opener.OpenMongo(connectionString);
+
         public MainModule()
         {
             // Root of the application, gather all submissions
             Get["/"] = _ =>
                 {
-                    return "Nancy HackerNews Main Page"; 
+                    var submissions = db.Submissions.All().OrderByVotesDescending().ToList(); 
+                    return View["views/list", submissions]; 
                 };
 
             // Redirection to the "submit" page
             Get["/submit"] = _ =>
                 {
-                    return "Submit a new link";
+                    return View["views/submit"];
                 };
 
             // Post action upon submit
             Post["/submit"] = _ =>
                 {
-                    return "Process the new submission";
+                    var submission = this.Bind<Submission>();
+                    db.Submissions.Insert(submission);
+                    return Response.AsRedirect("/");
                 };
 
             // Increase the score of the given submission
             Get["/voteup/{id}"] = parameters =>
                 {
-                    return "Vote up submission " + parameters.id;
+                    var submission = db.Submissions.FindById(parameters.id.ToString());
+                    submission.Votes++;
+                    db.Submissions.Update(submission);
+                    return Response.AsRedirect("/");
                 };
         }
     }
--- Demo_NancySelfHosting/SelfHosting/Program.cs
+++ Demo_NancySelfHosting/SelfHosting/Program.cs
@@ -6,13 +6,12 @@ namespace SelfHostingNancy
 {
 	class Program
 	{
-		private const string serverUri = "http://localhost:1234";
+		private const string serverUri = "http://localhost:12345";
 
 		static void Main(string[] args)
 		{
 			// initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
 			var host = new NancyHost(new Uri(serverUri));
-			Console.WriteLine("Listening on " + serverUri);
 			host.Start(); // start hosting
 			Console.ReadLine();
 			Console.WriteLine("You are about to close the application.");
